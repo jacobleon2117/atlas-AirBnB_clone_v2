c1f88b6acc5690b9924e894b11860a3e42da5b0a
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-03-12 23:49:00 -0500

    fixing

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module containing flask script with api route that displays 'HBNB'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    new_str = ""
+    for i in text:
+        if i == "_":
+            new_str = new_str + " "
+        else:
+            new_str = new_str + i
+    return 'C {}'.format(new_str)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    new_str = ""
+    for i in text:
+        if i == "_":
+            new_str = new_str + " "
+        else:
+            new_str = new_str + i
+    return "Python {}".format(new_str)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
